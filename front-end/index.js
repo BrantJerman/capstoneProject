a0834cdaf9f5e68cad32024ca753cb4250533c96
Brant Jerman <brantmichaeljerman@gmail.com>
2022-07-08 15:52:50 -0600

    Getting ready for Heroku

---
+++ front-end/index.js
@@ -0,0 +1,7 @@
+const btn1 = document.getElementById('btn1')
+const btn2 = document.getElementById('btn2')
+const btn3 = document.getElementById('btn3')
+
+// function btn1() {
+//     axios.get('http://localhost')
+// }
