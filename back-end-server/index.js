2a1ca5733eee6a4d5dd37c538878fa99d3a8cabd
Brant Jerman <brantmichaeljerman@gmail.com>
2022-07-08 16:46:56 -0600

    Finally, thanks Jared

--- back-end-server/index.js
+++ back-end-server/index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
+require.dotenv.config()
 
 const app = express()
 
-const port = process.env.PORT || 4005
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../front-end/webpage.html'))
+})
+
+app.get('/html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../front-end/page2.html'))
+})
+
+const port = process.env.PORT || 4000
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
