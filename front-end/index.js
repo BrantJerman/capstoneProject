e7ef5d0795ac2ee3f546f348d96b274ad84ed16a
Brant Jerman <brantmichaeljerman@gmail.com>
2022-07-11 14:16:54 -0600

    Finished html & css

--- front-end/index.js
+++ front-end/index.js
@@ -5,3 +5,56 @@ const btn3 = document.getElementById('btn3')
 // function btn1() {
 //     axios.get('http://localhost')
 // }
+
+function bstTrail () {
+    return `
+        <div class="bstTrail">
+        <p class="trailName">${trail.name}</p>
+        <p>Location: ${trail.location}</p>
+        <p>Difficulty: ${trail.difficulty} damage</p>
+        <p>Rating: ${trail.rating} damage</p>
+        <button class="trail-btn" onclick="hideTrail()">Hide Trail</button>
+        </div>
+    `
+}
+
+function gmTrail () {
+    return `
+        <div class="gmTrail">
+        <p class="trailName">${trail.name}</p>
+        <p>Location: ${trail.location}</p>
+        <p>Difficulty: ${trail.difficulty} damage</p>
+        <p>Rating: ${trail.rating} damage</p>
+        <button class="trail-btn2" onclick="hideTrail()">Hide Trail</button>
+        </div>
+    `
+}
+
+function erTrail () {
+    return `
+        <div class="erTrail">
+        <p class="trailName">${trail.name}</p>
+        <p>Location: ${trail.location}</p>
+        <p>Difficulty: ${trail.difficulty} damage</p>
+        <p>Rating: ${trail.rating} damage</p>
+        <button class="trail-btn3" onclick="hideTrail()">Hide Trail</button>
+        </div>
+    `
+}
+
+function submitTrail () {
+    if (trail.value < 1){
+        alert('Make sure to add the name of your trail!')
+        return
+    } else if (location.value < 1){
+        alert('Make sure to add the location of your trail!')
+        return
+    }
+
+    let userRating = document.querySelector('input[name="rating"]:checked').value
+    let body = {
+        name: nameInput.value, 
+        rating: +userRating, 
+        countryId: +countrySelect.value
+    }
+}
